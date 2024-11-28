260ab8cb398f3d0bf2ebd406a2341b1eb830b224
Chamber6821 <vania.osinin@yandex.ru>
2024-11-28 03:47:23 +0300

    Mount ring net

--- mount.py
+++ mount.py
@@ -1,17 +1,37 @@
 #!/bin/python3.12
 import os, sys, subprocess, signal
-bridge = subprocess.Popen(
-    ['socat', 'pty,raw,echo=0,link=SIDE1', 'pty,raw,echo=0,link=SIDE2'],
-    stdin=sys.stdin,
-    stdout=sys.stdout,
-    stderr=sys.stderr
-)
+
+
+def make_bridge(side1: str, side2: str):
+    return subprocess.Popen(
+        ['socat', f'pty,raw,echo=0,link={side1}', f'pty,raw,echo=0,link={side2}'],
+        stdin=sys.stdin,
+        stdout=sys.stdout,
+        stderr=sys.stderr
+    )
+
+
+bridges = [
+    make_bridge('from_1', 'to_2'),
+    make_bridge('from_2', 'to_3'),
+    make_bridge('from_3', 'to_1'),
+]
+
 try:
     os.system(r'''
-    tmux new "./transmitter.py SIDE1" \; \
-    split "./receiver.py SIDE2" \; \
-    selectp -t 0
+    tmux new "./transmitter.py from_1" \; \
+    split -h -p 67 "./transmitter.py from_2" \; \
+    split -h "./transmitter.py from_3" \; \
+    selectp -t 0 \; \
+    split "./receiver.py to_1" \; \
+    selectp -t 2 \; \
+    split "./receiver.py to_2" \; \
+    selectp -t 4 \; \
+    split "./receiver.py to_3" \; \
+    selectp -t 0 \; \
     ''')
 finally:
-    bridge.send_signal(signal.SIGINT)
-    bridge.wait()
+    for bridge in bridges:
+        bridge.send_signal(signal.SIGINT)
+    for bridge in bridges:
+        bridge.wait()
